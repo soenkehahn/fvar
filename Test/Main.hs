a2df9843da035954d5a35d41e026bfd48e4fb924
Sönke Hahn <shahn@joyridelabs.de>
2012-12-25 16:44:55 +0100

    tests: writeAndRead

--- Test/Main.hs
+++ Test/Main.hs
@@ -9,6 +9,7 @@ import Data.FVar.Core
 
 main = do
     quickCheck newAndRead
+    quickCheck writeAndRead
 
 data Data = Data Int
   deriving (Eq, Show)
@@ -25,3 +26,13 @@ newAndRead d =
         fvar <- newFVar t "file" d
         r <- readFVar t fvar
         return (d == r)
+
+writeAndRead :: Data -> Data -> Property
+writeAndRead a b =
+    morallyDubiousIOProperty $
+    withSystemTempDirectory "fvar-tests" $ \ root ->
+    withTransaction root $ \ t -> do
+        fvar <- newFVar t "file" a
+        writeFVar t fvar b
+        r <- readFVar t fvar
+        return (b == r)
